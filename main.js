c286bdc20326152626592d42931485406af73193
ysk556 <ts3820@gmail.com>
2020-07-24 18:39:20 +0900

    list 3-23

--- main.js
+++ main.js
@@ -8,9 +8,20 @@ function action(event) {
         var li = document.createElement('li');
         li.className = 'list-group-item';
         li.textContent = f1.value;
+        li.addEventListener("click", clickItem);
         list.appendChild(li);
         msg.textContent = '「' + f1.value + '」を追加しました。';
         f1.value = '';
     }
-    console.log("hello!");
+}
+
+function clickItem(event) {
+    var item = event.target;
+    var items = item.parentNode.childNodes;
+    for (var i = 0; i < items.length; i++) {
+        items[i].classList.remove('active');
+    }
+    item.classList.add('active');
+    var msg = document.querySelector('#msg');
+    msg.textContent = "selected: " + item.textContent;
 }
