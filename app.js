aae5542b9d5e92fd6b38e09eea6bcafbcb4d5148
ysk556 <ts3820@gmail.com>
2020-07-26 08:49:29 +0900

    list 4-14

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const https = require('https');
+const r1 = require('./getline.js');
 
-https.get('https://tuya-no.firebaseio.com/mydata.json', (res) => {
-  let body = '';
-  res.setEncoding('utf8');
+async function main() {
+  let msg = await r1.getline('type your message: ');
+  msg = '{"message":"' + msg + '"}';
 
-  res.on('data', (chunk) => {
-    body += chunk;
+  const options = {
+    hostname: 'tuya-no.firebaseio.com',
+    path: '/dummy.json',
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(msg)
+    }
+  };
+
+  let req = https.request(options, (res) => {
+    console.log('STATUS: ' + res.statusCode);
+  })
+
+  req.on('error', (e) => {
+    console.log(e);
   });
 
-  res.on('end', (res) => {
-    json = JSON.parse(body);
-    console.table(json);
+  req.write(msg, (err) => {
+    if (err != undefined) {
+      console.log(err);
+    }
   });
-})
+
+  req.end(() => {
+    console.log('finished!!');
+  });
+}
+main();
