b38ec086be1b35d6345a6753d9c8535d4004b5db
ysk556 <ts3820@gmail.com>
2020-07-25 17:45:05 +0900

    list 4-13

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const https = require('https');
 
-https.get('https://tuya-no.firebaseio.com/message.json', (res) => {
+https.get('https://tuya-no.firebaseio.com/mydata.json', (res) => {
   let body = '';
   res.setEncoding('utf8');
+
   res.on('data', (chunk) => {
     body += chunk;
   });
 
   res.on('end', (res) => {
-    console.log(body);
+    json = JSON.parse(body);
+    console.table(json);
   });
 })
