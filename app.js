cd554003f35d8d653cb2cc8c4d10c719994589ca
ysk556 <ts3820@gmail.com>
2020-07-25 17:18:55 +0900

    list 4-12

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
-var data = {
-  Tokyo: 1230,
-  Osaka: 987,
-  NewYork: 456
-};
-console.table(data);
+const https = require('https');
 
-var data2 = {
-  Windows: {
-    Tokyo: 1230,
-    Osaka: 987,
-    NewYork: 456
-  },
-  macOS: {
-    Tokyo: 543,
-    Osaka: 210,
-    NewYork: 789
-  },
-  Linux: {
-    Tokyo: 98,
-    Osaka: 76,
-    NewYork: 54
-  }
-};
-console.table(data2);
+https.get('https://tuya-no.firebaseio.com/message.json', (res) => {
+  let body = '';
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  res.on('end', (res) => {
+    console.log(body);
+  });
+})
