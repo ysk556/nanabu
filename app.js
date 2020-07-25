dcf3a2864d79d49aa6f14f535f6221689c90eb14
ysk556 <ts3820@gmail.com>
2020-07-25 16:13:25 +0900

    list 4-10

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
+const gl = require("./getline.js");
+
 const readline = require('readline');
 
-var counter = 0;
+async function main() {
+  while (true) {
+    var re = await gl.getline('input: ');
+    console.log('(' + re.length + ' chars)');
+  }
+}
 
-const read = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-read.on('line', (line) => {
-  counter++;
-  console.log('(' + counter + ':' + line.length + ' chars)');
-});
-
-var content = ["--begin--",
-  "これは、長文のコンテンツです。",
-  "長い文章を出力させる例です。",
-  "どのように表示されるかな？",
-  "--end--"
-].join('\n');
-read.write(content);
-read.close();
+main();
