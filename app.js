9514d5802753bfad40be0ae71d06bcb86c80ca3e
ysk556 <ts3820@gmail.com>
2020-07-26 09:08:13 +0900

    list 4-15

--- app.js
+++ app.js
@@ -1,36 +1,4 @@
-const https = require('https');
-const r1 = require('./getline.js');
+const fs = require("fs");
 
-async function main() {
-  let msg = await r1.getline('type your message: ');
-  msg = '{"message":"' + msg + '"}';
-
-  const options = {
-    hostname: 'tuya-no.firebaseio.com',
-    path: '/dummy.json',
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': Buffer.byteLength(msg)
-    }
-  };
-
-  let req = https.request(options, (res) => {
-    console.log('STATUS: ' + res.statusCode);
-  })
-
-  req.on('error', (e) => {
-    console.log(e);
-  });
-
-  req.write(msg, (err) => {
-    if (err != undefined) {
-      console.log(err);
-    }
-  });
-
-  req.end(() => {
-    console.log('finished!!');
-  });
-}
-main();
+const f1 = fs.readFileSync("./index.html");
+console.log(f1.toString());
